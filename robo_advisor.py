1a60e0d68c7ba068959fff33340857f032ff41a9
zky44 <77955005+zky44@users.noreply.github.com>
2021-03-01 23:45:51 -0500

    Added latest_close

--- robo_advisor.py
+++ robo_advisor.py
@@ -2,19 +2,26 @@
 
 import requests
 import json
+import datetime
+
+date = datetime.date.today()
+time = datetime.datetime.now()
+date_time = date, time.strftime("%I:%M:%S %p")
+
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
 
 # INFO INPUTS
 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 response = requests.get(request_url)
-#print(type(response)) # class 'requests.models.Response'
-#print(response.status_code) # how successful this request was (#200)
-#print(response.text) # actual body of the request 
+
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+latest_close = parsed_response["Time Series (Daily)"]["2019-02-20"]["4.close"]
 
 #breakpoint()
 
@@ -24,10 +31,10 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print(f"REQUEST AT: {date_time}") #need to fix this
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
