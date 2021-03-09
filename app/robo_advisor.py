e0b13bdfaadbbbd2a2a800edd9988728c71281fc
zky44 <77955005+zky44@users.noreply.github.com>
2021-03-08 22:00:26 -0500

    Data validation part 2
    Used an if-in statement to validate if the stock symbol exists

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -5,32 +5,69 @@ import json
 import os
 from dotenv import load_dotenv
 
+
 import requests
 from datetime import datetime
+import validators
+
 
 load_dotenv()
 
 now = datetime.now()
-print(now.strftime('%Y/%m/%d %I:%M:%S%p'))
+#print(now.strftime('%Y/%m/%d %I:%M:%S%p'))
 request_time = now.strftime('%Y/%m/%d %I:%M:%S%p')
-
-#date = datetime.date.today()
-#time = datetime.datetime.now()
-#date_time = date, time.strftime("%I:%M:%S %p")
 
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price)
 
 # INFO INPUTS
 
-symbol = input("Please input a stock symbol in all upper-case letters: ")
+user_choice = input("Please input a stock symbol: ")
+user_choice = user_choice.upper()
+
+#print(user_choice.isdigit())
+
+#preliminary data validation
+#Step 1 - test that user input is not too long
+if len(user_choice) < 7:
+    pass
+else:
+    print("Oops, we are expecting a properly formatted stock symbol such as 'MSFT'. Please try again.")
+    exit()
+
+#Step 2 - test that user input does not contain any numbers
+contains_digit = False
+for character in user_choice:
+    if character.isdigit():
+        contains_digit = True
+
+if contains_digit == False:
+    pass
+else:
+    print("Oops, we are expecting a properly formatted stock symbol such as 'MSFT'. Please try again.")
+    exit()
+
+
+symbol = user_choice
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+
 response = requests.get(request_url)
 
-
 parsed_response = json.loads(response.text)
+
+
+
+#data validation part 2 - validate that stock symbol exists
+#found an online forum helping explain how to look for a specific value in a dictionary (https://www.educative.io/edpresso/how-to-check-if-a-key-exists-in-a-python-dictionary)
+key_to_lookup = 'Error Message'
+if key_to_lookup in parsed_response:
+  print ("Sorry, we couldn't find any trading data for that symbol. Please try again.")
+  exit()
+else:
+  pass
+
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
@@ -86,7 +123,7 @@ print("-------------------------")
 print(f"SELECTED SYMBOL: {symbol}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print(f"REQUEST AT: {request_time}") #need to fix this
+print(f"REQUEST AT: {request_time}")
 print("-------------------------")
 print(f"LATEST DAY: {dates[0]}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
